5487a524d78af77859156e31f26bb2b5687cbb9e
hypotermia <zakkiaja98@gmail.com>
2023-11-30 17:41:12 +0700

    create CSRF

--- Program.cs
+++ Program.cs
@@ -9,7 +9,11 @@ builder.Services.AddDbContext<TaskDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
-
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.SameSite = SameSiteMode.Strict; // or SameSiteMode.Lax
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always; // Requires HTTPS
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
